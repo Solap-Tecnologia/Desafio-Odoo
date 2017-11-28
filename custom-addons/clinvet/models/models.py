38092f45cde9f316ae838c954620a2835dfcd365
Rodrigo Ferreira <rodrigo.rodrigues104@etec.sp.gov.br>
2017-11-28 15:24:27 -0200

    Add many2many_tags na consulta e alterações no model (Consulta e Servico para Many2many)

--- custom-addons/clinvet/models/models.py
+++ custom-addons/clinvet/models/models.py
@@ -7,8 +7,8 @@ class TipoDeAnimal(models.Model):
 	_description = 'Tipo de Animal'
 
 	id = fields.Integer()
-	name = fields.Char(string="Especie")
-	raca = fields.Char(string="Raca")
+	name = fields.Char(string="Espécie")
+	raca = fields.Char(string="Raça")
 	animal_ids = fields.One2many('vetclin.animal', 'tipodeanimal_id', string="Animais")
 
 class Servico(models.Model):
@@ -18,28 +18,30 @@ class Servico(models.Model):
 	id = fields.Integer()
 	name = fields.Char(string="Serviço")
 	descricao = fields.Text(string="Descrição do Serviço")
-	consulta_id = fields.Many2one('vetclin.consulta', ondelete='cascade', string="Consulta")
+	preco = fields.Float(string="Preço", digits=(7,2))
+	#consulta_id = fields.Many2one('vetclin.consulta', ondelete='cascade', string="Consulta")
 
 class Consulta(models.Model):
 	_name = 'vetclin.consulta'
 	_description = 'Consulta'
 
 	id = fields.Integer()
-	observacao = fields.Char(string="Observações")
-	procedimento_id = fields.One2many('vetclin.servico' , 'consulta_id', string="Serviço")
-	medicamento_id = fields.One2many('vetclin.medicamento' , 'consulta_id', string="Medicamento")
+	observacao = fields.Text(string="Observações")
+	servicos_id = fields.Many2many('vetclin.servico')
+	#medicamento_id = fields.One2many('vetclin.medicamento' , 'consulta_id', string="Medicamento")
 	animal_id = fields.Many2one('vetclin.animal', ondelete='cascade', string="Animal")		
-	#veterinario_id = fields.Many2one('res.partner', ondelete='cascade', string="veterinario")
+	veterinario_id = fields.Many2one('res.partner', ondelete='cascade', string="Veterinário")
 	consultorio_id = fields.Many2one('vetclin.consultorio', ondelete='cascade', string="Consultorio")		
 
-class Medicamento(models.Model):
-	_name = 'vetclin.medicamento'
-	_description = 'Medicamento'
-
-	id = fields.Integer()
-	name = fields.Char(string="Medicamento")
-	comp_quimica = fields.Char(string="Composição Química")
-	consulta_id = fields.Many2one('vetclin.consulta', ondelete='cascade', string="Medicamento")
+''' Relacionar com a classe Produto '''
+#class Medicamento(models.Model):
+#	_name = 'vetclin.medicamento'
+#	_description = 'Medicamento'
+#
+#	id = fields.Integer()
+#	name = fields.Char(string="Medicamento")
+#	comp_quimica = fields.Text(string="Composição Química")
+#	consulta_id = fields.Many2one('vetclin.consulta', ondelete='cascade', string="Medicamento")
 
 class Animal(models.Model):
 	_name = 'vetclin.animal'
@@ -51,13 +53,14 @@ class Animal(models.Model):
 	cliente_id = fields.Many2one('res.partner', ondelete='cascade', string="Dono")
 	consulta_id = fields.One2many('vetclin.consulta' , 'animal_id', string="animal")
 
-class Produto(models.Model):
-	_name = 'vetclin.produto'
-	_description = 'Produto'
-
-	id = fields.Integer()
-	nome_produto = fields.Char(string="Produto")
-	desc_produto = fields.Char(string="Descrição do Produto")
+''' Extender o model product.template '''
+#class Produto(models.Model):
+#	_name = 'vetclin.produto'
+#	_description = 'Produto'
+#
+#	id = fields.Integer()
+#	name = fields.Char(string="Nome")
+#	descricao = fields.Text(string="Descrição do Produto")
 
 class Clinica(models.Model):
     _inherit =  'res.company'
@@ -71,7 +74,7 @@ class Consultorio(models.Model):
     _description = 'Consultorio'
 
     id = fields.Integer()
-    nome = fields.Text(string="Nome")
+    name = fields.Text(string="Nome")
     descricao = fields.Text(string="Descrição")
     ramal = fields.Char(string="Ramal")
     clinica_id = fields.Many2one('res.company', ondelete='cascade')
@@ -92,8 +95,9 @@ class Partner(models.Model):
 	data_demissao = fields.Date(string="Data de demissao")
 	is_veterinario = fields.Boolean(string="Veterinario", readonly=True, default=False)
 	#ja existentes
-	email = fields.Char(string="Email")
-	phone = fields.Char(string="Telefone")	
-	mobile = fields.Char(string="Celular")
+	email = fields.Char(string="Email", placeholder="Ex.: email@dominio.com")
+	phone = fields.Char(string="Telefone", placeholder="Ex.: (00) 00000-0000")	
+	mobile = fields.Char(string="Celular", placeholder="Ex.: (00) 0000-0000")
 	#relações
 	animal_ids = fields.One2many('vetclin.animal', 'cliente_id', string="Animais")
+	consulta_ids = fields.One2many('vetclin.consulta', 'veterinario_id', string="Veterinários")
